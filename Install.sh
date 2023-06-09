c8bcf88d0b7a72ce048bdbe0eefbdf3af665e720
TPM-28 <77407722+felix068@users.noreply.github.com>
2023-06-09 07:33:46 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -x "$(command -v wget)" ]; then
+if which wget >/dev/null 2>&1; then
     sudo wget -P "/usr/bin/" "https://raw.githubusercontent.com/felix068/PDF-installer/main/bin/Release/net7.0/linux-x64/publish/pdf"
     sudo chmod +x "/usr/bin/pdf"
-elif [ -x "$(command -v curl)" ]; then
+elif which curl >/dev/null 2>&1; then
     sudo curl -o "/usr/bin/pdf" "https://raw.githubusercontent.com/felix068/PDF-installer/main/bin/Release/net7.0/linux-x64/publish/pdf"
     sudo chmod +x "/usr/bin/pdf"
 else
